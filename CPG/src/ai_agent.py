e48586054aee0836227c2872684d6789cdd7b447
Chetanand Meher <chetanandmeher1282004@gmail.com>
2026-04-22 23:54:53 +0530

    change the loading methods of the groq api key from os to streamlit secrets

--- CPG/src/ai_agent.py
+++ CPG/src/ai_agent.py
@@ -1,11 +1,10 @@
 import os
 from groq import Groq
 from dotenv import load_dotenv
-
+import streamlit as st
 load_dotenv()
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 # ==============================================================================
 # 🔥 CORE: MULTI-SIGNAL OPTIMAL DISCOUNT PREDICTOR
